73106261ad0d37ced641100b6ef67f49ce95a08b
Tiago <tiagohslino@gmail.com>
2024-12-18 16:45:05 -0300

    commit vs-git 16h45 181224

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function selecao(opcao){
+function selecionarComida(opcao){
 
     const selecionado = document.querySelector("." + opcao);
+    const selecionadoAntes = document.querySelector(".bordaVerde");
+
+    if (selecionadoAntes !== null) {
+        selecionadoAntes.classList.remove("bordaVerde");
+    }
+    
+    selecionado.classList.add("bordaVerde");
     
     
 }
